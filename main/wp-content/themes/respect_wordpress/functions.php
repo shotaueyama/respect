2c5a067d375c313c5e8478ead7fdc6a0448572f3
shotaueyama <shota.ueyama@gmail.com>
2026-07-07 04:49:19 +0900

    Add Google Analytics tracking script

--- main/wp-content/themes/respect_wordpress/functions.php
+++ main/wp-content/themes/respect_wordpress/functions.php
@@ -3098,3 +3098,21 @@ function rf_theme_send_product_inquiry_email($data) {
 
     return wp_mail($recipient, $subject, rf_theme_render_product_inquiry_mail_body($data), $headers);
 }
+
+function rf_theme_print_google_analytics() {
+    ?>
+<script>
+  (function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+      (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date(); a = s.createElement(o),
+      m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+  ga('create', 'UA-69273372-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
+    <?php
+}
+add_action('wp_head', 'rf_theme_print_google_analytics', 20);
